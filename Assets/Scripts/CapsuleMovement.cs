b6d16a00c9cfdd5ddd17ae432b3d43b8600c1680
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2020-09-22 20:28:02 -0400

    push
    Initial Files

---
+++ Assets/Scripts/CapsuleMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int b = 0;
+    }
+}
