37fb04fe5835f623571ade598f3b010d06f48f7e
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2020-09-29 19:54:11 -0400

    Script edits

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,12 +7,12 @@ public class CapsuleMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int a = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        int b = 0;
+
     }
 }
