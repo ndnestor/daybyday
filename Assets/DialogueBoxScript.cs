2543ce57c947ffcfa43b877c139f143b7bfcf6e5
kyleshteynberg <56613539+kyleshteynberg@users.noreply.github.com>
2020-10-13 19:16:39 -0400

    Added dialogue box

---
+++ Assets/DialogueBoxScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBoxScript : MonoBehaviour
+{
+    public GameObject BoxImage;
+    public Text dialogue;
+    public string[] textLines;
+    public int currentLine = 0;
+
+    public void Start()
+    {
+        dialogue.text = textLines[0];
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            NextLine();
+        }
+    }
+
+    public void NextLine()
+    {
+        currentLine++;
+        if (currentLine == textLines.Length)
+        {
+            currentLine = 0;
+            HideDialogueBox();
+        }
+        else
+        {
+            dialogue.text = textLines[currentLine];
+        }
+    }
+
+    public void ShowDialogueBox()
+    {
+        BoxImage.SetActive(true);
+    }
+
+    public void HideDialogueBox()
+    {
+        BoxImage.SetActive(false);
+    }
+}
