6eb06aab1653058385a65b7287bd6f31215da35c
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2021-01-18 17:15:03 -0500

    Major Adjustments
    -Resprited everything
    -Colliders and Triggers implemented
    -Grouped Sprites

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -5,29 +5,20 @@ using UnityEngine;
 public class Movement2D : MonoBehaviour
 {
     public Rigidbody2D rb;
-    [SerializeField] private float moveSpeed = 5;
+    public float moveSpeed = 5f;
 
     private Vector2 movement;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        //rb.velocity = movement * moveSpeed;
-        //rb.position += movement * moveSpeed;
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
 }
