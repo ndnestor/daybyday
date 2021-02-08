bf3793eee7f7897363b63ec539b23a2d51cdf1f0
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2021-02-08 18:00:47 -0500

    Computer Interface Progress

--- Assets/Scripts/Computer/ComputerInterface.cs
+++ Assets/Scripts/Computer/ComputerInterface.cs
@@ -7,11 +7,27 @@ public class ComputerInterface : MonoBehaviour
     public GameObject mainRoom;
     public GameObject computerScreen;
 
+    public GameObject profilePage;
+    public GameObject inboxPage;
+    public GameObject assignmentPage;
+    public GameObject libraryPage;
+    public GameObject blasterPage;
+    public GameObject helpPage;
+    public GameObject settingsPage;
+
+    public GameObject currentPage;
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
         {
             QuitComputer();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click, casting ray.");
+            CastRay();
         }
     }
 
@@ -21,5 +37,35 @@ public class ComputerInterface : MonoBehaviour
         computerScreen.SetActive(false);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void CastRay()
+    {
+        //Casts a ray and calls the ChangePage() method
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 100);
+        if (hit)
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            Debug.Log("Hit object: " + hit.collider.gameObject.name);
+
+            currentPage = ChangePage(hit.collider.gameObject.name);
+        }
+    }
+
+    private GameObject ChangePage(string newName)
+    {
+        GameObject newPage = null;
+
+        if(newName.Equals("Assignments Button"))
+        {
+            assignmentPage.SetActive(true);
+            helpPage.SetActive(false);
+
+            currentPage = assignmentPage;
+        }
+
+        return newPage;
     }
 }
