f42285d346b7b50a52d6475a2b141918a3ad27df
ndnestor <Nathan.nestor@outlook.com>
2021-05-04 21:17:28 -0400

    Added test piano functionality

---
+++ Assets/Scripts/Piano.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FluidMidi;
+
+public class Piano : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            print("Playing sound");
+            GetComponent<SongPlayer>().enabled = true;
+            GetComponent<SongPlayer>().Play();
+        }
+        else if(!Input.GetKey(KeyCode.J))
+        {
+            GetComponent<SongPlayer>().Stop();
+            GetComponent<SongPlayer>().enabled = false;
+        }
+    }
+}
