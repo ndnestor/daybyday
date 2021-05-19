b9b61399c126140e2b98434210829d27adb1624f
ndnestor <Nathan.nestor@outlook.com>
2021-05-18 21:41:45 -0400

    Added foundation for neglected sprites

--- Assets/Scripts/InteractionHandler.cs
+++ Assets/Scripts/InteractionHandler.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class InteractionHandler : MonoBehaviour
 {
 
-	private Hashtable registeredObjects = new Hashtable();
+	private readonly Hashtable registeredObjects = new Hashtable();
+	// Used for objects' neglected appearance
+	[SerializeField] private Sprite neglectedPianoSprite;
+	[SerializeField] private Sprite neglectedYogaMatSprite;
+	[SerializeField] private Sprite neglectedComputerSprite;
+	[SerializeField] private Sprite neglectedBookshelfSprite;
+	[SerializeField] private Sprite neglectedWindowSprite;
+	[SerializeField] private Sprite neglectedWindowLightSprite;
+	private Hashtable objectNeglection = new Hashtable(); // Key: string | Value: bool
 
-    [HideInInspector] public static InteractionHandler Instance;
+	[HideInInspector] public static InteractionHandler Instance;
 
 	private void Awake()
 	{
@@ -20,26 +28,43 @@ public class InteractionHandler : MonoBehaviour
 	{
 		if(registeredObjects.ContainsKey(objectName))
 		{
-			Debug.LogWarning("Object " + objectName + " was already registerd");
+			Debug.LogWarning("Object " + objectName + " was already registered");
 			return false;
 		}
 		else
 		{
 			registeredObjects.Add(objectName, objectAction);
+			objectNeglection.Add(objectAction, true);
 			return true;
 		}
 	}
 
-	// Calls an interaction object's action give the name of it
+	// Calls an interaction object's action given the name of it
 	public bool Interact(string objectName)
 	{
 		System.Action action = (System.Action)registeredObjects[objectName];
 		if(action != null)
 		{
 			action();
+			objectNeglection[objectName] = false;
 			return true;
 		}
 		Debug.LogWarning("Could not interact because the given object name is not registered");
 		return false;
+	}
+
+	// This should be called at the beginning of every day
+	public void UpdateNeglectedSprites()
+	{
+		foreach(string objectName in objectNeglection.Keys)
+		{
+			if((bool)objectNeglection[objectName])
+			{
+				switch(objectName) {
+					case "Piano":
+						// TODO: Finish this
+				}
+			}
+		}
 	}
 }
