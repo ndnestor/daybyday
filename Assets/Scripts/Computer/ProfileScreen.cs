19a31e82fed0c1cbd126bc08d8aef8b5c9e95c32
ndnestor <Nathan.nestor@outlook.com>
2021-06-10 22:15:24 -0400

    Basic same order profile screen functionality

--- Assets/Scripts/Computer/ProfileScreen.cs
+++ Assets/Scripts/Computer/ProfileScreen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
@@ -20,26 +21,81 @@ public class ProfileScreen : MonoBehaviour {
     [SerializeField] private Color windowColor;
     [SerializeField] private Color otherColor;
 
-    // TODO: Remove this magic number
-    private float[] barHeights = new float[10];
-    private const int BAR_CHART_SORTING_ORDER = 1;
+    [SerializeField] private GameObject chronologicalBars;
+    [SerializeField] private GameObject sameOrderBars;
+
+    private float[] barHeights;
+    private const int BAR_SPRITE_SORTING_ORDER = 1;
     private bool isChronological;
 
-    private void UpdateBarChart(string activityName, int timeUsed) {
+    private readonly Hashtable activityTimes = new Hashtable();
+    
+    private enum Interaction {
+        Plant,
+        Piano,
+        YogaMat,
+        Computer,
+        Bookshelf,
+        Window,
+        Other
+    }
+    
+    private void Start() {
+        barHeights = new float[Tracking.MAX_DAYS];
 
+        activityTimes[Interaction.Plant] = 0;
+        activityTimes[Interaction.Piano] = 0;
+        activityTimes[Interaction.YogaMat] = 0;
+        activityTimes[Interaction.Computer] = 0;
+        activityTimes[Interaction.Bookshelf] = 0;
+        activityTimes[Interaction.Window] = 0;
+        activityTimes[Interaction.Other] = 0;
+    }
+
+    private void ToggleBarChartMode() {
+        isChronological = !isChronological;
+        
+        chronologicalBars.SetActive(isChronological);
+        sameOrderBars.SetActive(!isChronological);
+        
+        // TODO: Determine if above code works. If not, use the below code but modify as it does not work currently
+        /*foreach(SpriteRenderer spriteRenderer in chronologicalBarRenderers) {
+            spriteRenderer.enabled = isChronological;
+        }
+        foreach(SpriteRenderer spriteRenderer in sameOrderBarRenderers) {
+            spriteRenderer.enabled = !isChronological;
+        }*/
+    }
+    
+    private void UpdateBarChart(Interaction interaction, int timeUsed) {
+        
+        // Update the chronological bar chart
+        UpdateBarChartChronological(interaction, timeUsed);
+        
+        // Update the same order bar chart
+        UpdateBarChartSameOrder(interaction, timeUsed);
+    }
+
+    // TODO: Switch to enum based activity name
+    private void UpdateBarChartChronological(Interaction interaction, int timeUsed) {
         // Create the new bar chart section
         GameObject newBarChartSection = Instantiate(barChartSectionPrefab, barChartOrigin.position, Quaternion.identity,
-            barChartOrigin);
+            chronologicalBars.transform);
+        newBarChartSection.name = "Chronological Bar Chart Section";
         
-        // Set its sorting order to be sorted in front of the computer
-        newBarChartSection.GetComponent<SpriteRenderer>().sortingOrder = BAR_CHART_SORTING_ORDER;
+        // Set its sprite renderer properties
+        SpriteRenderer newSpriteRenderer = newBarChartSection.GetComponent<SpriteRenderer>();
+        if(!isChronological) {
+            newSpriteRenderer.enabled = false;
+        }
+        newSpriteRenderer.sortingOrder = BAR_SPRITE_SORTING_ORDER;
 
         // Set its dimensions
         float barHeight = timeUsed * timeUnitHeight;
         newBarChartSection.transform.localScale = new Vector3(barWidth, barHeight, 1);
         
         // Set its location
-        int dayNum = Tracking.Instance.dayNum;
+        int dayNum = Tracking.Instance.DayNum;
         float xPos = (barPadding + barWidth) * (dayNum - 1);
         float yPos = barHeights[dayNum - 1] + 0.5f * barHeight;
         newBarChartSection.transform.localPosition = new Vector3(xPos, yPos, 0);
@@ -48,42 +104,92 @@ public class ProfileScreen : MonoBehaviour {
         barHeights[dayNum - 1] = yPos + 0.5f * barHeight;
         
         // Set its color
-        Color barChartSectionColor;
-        switch(activityName) {
-            case "piano":
-                barChartSectionColor = pianoColor;
-                break;
-            case "yoga mat":
-                barChartSectionColor = yogaMatColor;
-                break;
-            case "computer":
-                barChartSectionColor = computerColor;
-                break;
-            case "bookshelf":
-                barChartSectionColor = bookshelfColor;
-                break;
-            case "plant":
-                barChartSectionColor = plantColor;
-                break;
-            case "window":
-                barChartSectionColor = windowColor;
-                break;
-            case "other":
-                barChartSectionColor = otherColor;
-                break;
-            default:
-                Debug.LogError("Invalid activity name. Setting bar color to magenta");
-                barChartSectionColor = Color.magenta;
-                break;
+        newBarChartSection.GetComponent<SpriteRenderer>().color = GetInteractionColor(interaction);
+    }
+
+    private void UpdateBarChartSameOrder(Interaction interaction, int timeUsed) {
+        activityTimes[interaction] = (int)activityTimes[interaction] + timeUsed;
+
+        // Clear bar chart
+        Transform sameOrderBarTransform = sameOrderBars.transform;
+
+        Transform currDaySection;
+        if(sameOrderBarTransform.childCount < Tracking.Instance.DayNum) {
+            currDaySection = new GameObject().transform;
+            currDaySection.parent = sameOrderBarTransform;
+            currDaySection.localPosition = Vector3.zero;
+            currDaySection.name = "Day " + Tracking.Instance.DayNum;
+        } else {
+            currDaySection = sameOrderBarTransform.GetChild(Tracking.Instance.DayNum - 1);
         }
-        newBarChartSection.GetComponent<SpriteRenderer>().color = barChartSectionColor;
+
+        if(currDaySection.childCount >= Tracking.Instance.DayNum) {
+            for(int i = 0; i < currDaySection.childCount; i++) {
+                Destroy(currDaySection.GetChild(i).gameObject);
+            }
+        }
+        
+        Debug.Log("===============");
+
+        // Loop through every interaction type
+        float totalBarHeight = 0;
+        foreach(Interaction currInteraction in Enum.GetValues(typeof(Interaction))) {
+
+            // Create the new bar chart section
+            GameObject newBarChartSection = Instantiate(barChartSectionPrefab, barChartOrigin.position, Quaternion.identity,
+                currDaySection);
+            newBarChartSection.name = "Same Order Bar Chart Section";
+            
+            // Set its sprite renderer properties
+            SpriteRenderer newSpriteRenderer = newBarChartSection.GetComponent<SpriteRenderer>();
+            if(isChronological) {
+                newSpriteRenderer.enabled = false;
+            }
+            newSpriteRenderer.sortingOrder = BAR_SPRITE_SORTING_ORDER;
+
+            // Set its dimensions
+            float barHeight = (int)activityTimes[currInteraction] * timeUnitHeight;
+            newBarChartSection.transform.localScale = new Vector3(barWidth, barHeight, 1);
+        
+            // Set its location
+            int dayNum = Tracking.Instance.DayNum;
+            float xPos = (barPadding + barWidth) * (dayNum - 1);
+            float yPos = totalBarHeight + 0.5f * barHeight;
+            Debug.Log(yPos);
+            newBarChartSection.transform.localPosition = new Vector3(xPos, yPos, 0);
+            
+            newSpriteRenderer.color = GetInteractionColor(currInteraction);
+
+            totalBarHeight += barHeight;
+        }
+    }
+
+    private Color GetInteractionColor(Interaction interaction) {
+        switch(interaction) {
+            case Interaction.Bookshelf:
+                return bookshelfColor;
+            case Interaction.Plant:
+                return plantColor;
+            case Interaction.Piano:
+                return pianoColor;
+            case Interaction.YogaMat:
+                return yogaMatColor;
+            case Interaction.Computer:
+                return computerColor;
+            case Interaction.Window:
+                return windowColor;
+            case Interaction.Other:
+                return otherColor;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
     private void Update() {
         if(Input.GetKeyDown(KeyCode.B)) {
             string[] activityNames = {"plant", "piano", "yoga mat", "computer", "bookshelf", "window", "other"};
-            int randomIndex = (int)Random.Range(0, activityNames.Length - 1);
-            UpdateBarChart(activityNames[randomIndex], Random.Range(1, 4));
+            int randomIndex = Random.Range(0, activityNames.Length - 1);
+            UpdateBarChart((Interaction)Enum.GetValues(typeof(Interaction)).GetValue(randomIndex), Random.Range(1, 4));
         }
     }
 }
