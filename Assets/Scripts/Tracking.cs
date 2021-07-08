7a71daab291444171fa389a7ee09f29d6c8d3eb2
ndnestor <Nathan.nestor@outlook.com>
2021-07-08 19:49:49 -0400

    Unified curly brace style

--- Assets/Scripts/Tracking.cs
+++ Assets/Scripts/Tracking.cs
@@ -123,15 +123,18 @@ public class Tracking : MonoBehaviour
             if(DayNum == 2)
             {
                 agendasBox.SetActive(true);
-                Movement2D.Instance.MoveTo(agendasBox.transform.position + Vector3.left,
-                    () => { MainInstances.Get<DialogueSystem>().Present(agendaDeliverDialogue); });
+                Movement2D.Instance.MoveTo(agendasBox.transform.position + Vector3.left, () =>
+                {
+                    MainInstances.Get<DialogueSystem>().Present(agendaDeliverDialogue);
+                });
             }
         }
 
         Movement2D.Instance.MoveTo(bedDestination.position, CallbackAction);
     }
 
-    private IEnumerator FadeToBlack(int fadeSpeed) {
+    private IEnumerator FadeToBlack(int fadeSpeed)
+    {
         SpriteRenderer spriteRenderer = blackOverlay.GetComponent<SpriteRenderer>();
         while(spriteRenderer.color.a < 1) {
             Color currColor = spriteRenderer.color;
