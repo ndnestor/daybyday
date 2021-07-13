470b965019645d0a37a9cfd8cbc5f2dddfe88c4b
sorrer <sorrer@gmail.com>
2021-07-13 01:32:56 -0400

    Added string registry and dialogue node usage

---
+++ Assets/Scripts/Dialogue/DialogueGraph/Nodes/Registry/StringCompareNode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Dialogue.Nodes.Registry
+{
+    [NodeTint(COLOR_MISC_2)]
+    [NodeWidth(350)]
+    [CreateNodeMenu("Registry/StringCompare")]
+    public class StringCompareNode : DialogueNodeBase
+    {
+        [Tooltip("Registry name to test")]
+        public string RegistryName;
+        
+        [Tooltip("Registry name to compare against")]
+        public string OtherRegistry;
+
+        [Input(connectionType = ConnectionType.Override)] public Empty enter;
+        [Output(connectionType = ConnectionType.Override)] public Empty exitTrue;
+        [Output(connectionType = ConnectionType.Override)] public Empty exitFalse;
+        public override void MoveNext() {
+
+            DialogueGraph fmGraph = graph as DialogueGraph;
+
+            string exitPortName = "exitTrue";
+            string argumentValue = fmGraph.stringRegistry.Get(RegistryName);
+            if (!argumentValue.Equals(fmGraph.stringRegistry.Get(OtherRegistry))) {
+                exitPortName = "exitFalse";
+            }
+
+            MoveNext(exitPortName);
+        }
+
+        public override void OnEnter() {
+            base.OnEnter();
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueGraph/Nodes/Registry/StringSetterNode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Dialogue.Nodes.Registry
+{
+    [NodeTint(COLOR_MISC_2)]
+    [NodeWidth(350)]
+    [CreateNodeMenu("Registry/StringSetter")]
+    public class StringSetterNode : DialogueNodeBase
+    {
+        [Tooltip("Registry name to set")]
+        public string RegistryName;
+        [Tooltip("Registry value to set to")]
+        [TextArea]public string RegistryValue;
+
+        [Input(connectionType = ConnectionType.Override)] public Empty enter;
+        [Output(connectionType = ConnectionType.Override)] public Empty exit;
+		
+        public override void MoveNext() {
+            base.MoveNext();
+
+        }
+
+        public override void OnEnter() {
+            base.OnEnter();
+            
+            var stringRegistry = ((DialogueGraph)graph).stringRegistry;
+            
+            stringRegistry.Set(RegistryName, RegistryValue);
+        }
+    }
+}
