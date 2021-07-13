9b263293411c75601bdb180753682b85485ef286
ndnestor <Nathan.nestor@outlook.com>
2021-07-13 19:50:08 -0400

    Implemented dialogue graph in Productivity Aid prompt method. Fixed curly brace formatting

--- Assets/Scripts/ProductivityAid.cs
+++ Assets/Scripts/ProductivityAid.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using Game;
 using Game.Dialogue;
+using Game.Registry;
 using UnityEngine;
 
-public class ProductivityAid : MonoBehaviour {
+public class ProductivityAid : MonoBehaviour
+{
 
+    [SerializeField] private DialogueGraph dialogueGraph;
     [SerializeField] private int[] levelIncrementDays;
 
     [HideInInspector] public static ProductivityAid Instance { get; private set; }
 
+    private const string StringRegistryId = "Productivity Aid Prompt";
+    
+    private StringRegistry stringRegistry;
     private DialogueSystem dialogueSystem;
     private int numGoalsToComplete = 0;
     private int level = 0;
@@ -19,6 +25,7 @@ public class ProductivityAid : MonoBehaviour {
     {
         Instance = this;
         dialogueSystem = MainInstances.Get<DialogueSystem>();
+        stringRegistry = MainInstances.Get<StringRegistry>();
     }
 
     //Called at the start of each day
@@ -33,12 +40,14 @@ public class ProductivityAid : MonoBehaviour {
     //Shows relevant information about an interaction before starting it
     public void Prompt(string activityName, int activityTime, int proValue, int socValue, int casValue) {
         
-        //Create the message to send
+        //Create the message to send depending on current level
         string message = "";
-        if(level >= 1) {
+        if(level >= 1)
+        {
             message += $"The {activityName} will take {activityTime} hours to complete";
         }
-        if(level >= 2) {
+        if(level >= 2)
+        {
             message += $"\nProfessional value: {proValue}\nSocial value: {socValue}\nCasual value: {casValue}";
         }
         if(level == 3)
@@ -47,13 +56,17 @@ public class ProductivityAid : MonoBehaviour {
             {
                 message += "\nYou cannot do social or casual activities until you have completed all of Agenda's goals";
             } else {
-                //? Possibly change the message otherwise
+                //TODO: Possibly change the message otherwise
             }
         } else if(level == 4) {
             // TODO: Ask Eric about this
         }
         
-        //Display the message
+        //Set productivity aid string value in the string registry
+        stringRegistry.Set(StringRegistryId, message);
         
+        //Display the prompt
+        dialogueSystem.Present(dialogueGraph);
+
     }
 }
