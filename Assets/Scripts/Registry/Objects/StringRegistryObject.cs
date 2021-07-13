de46c6efe75fd1fbba105dbcf0c9e9369158a83d
sorrer <sorrer@gmail.com>
2021-07-13 01:34:53 -0400

    Added string registry object that was missing

---
+++ Assets/Scripts/Registry/Objects/StringRegistryObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Registry
+{
+    [CreateAssetMenu(fileName = "StringRegistry", menuName = "Objects/StringRegistry")]
+    public class StringRegistryObject : RegistryObject<string>
+    {
+    }
+}
