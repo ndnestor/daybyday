048f164163757f54015d584b99d991f4a3742413
ndnestor <Nathan.nestor@outlook.com>
2021-08-16 20:43:17 -0400

    Finished implemented textbox fill method

--- Assets/Scripts/Bookshelf/Book.cs
+++ Assets/Scripts/Bookshelf/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -18,34 +19,68 @@ public class Book : MonoBehaviour
 
 	public void OpenBook()
 	{
+		Debug.Log($"Opening book {title} by {author}");
 		openBookSprite.SetActive(true);
 		string remainingText = FillTextbox(leftPageText, 100, contents);
 		FillTextbox(rightPageText, 100, remainingText);
 	}
 
+	private void Update() {
+		if(Input.GetKeyDown(KeyCode.B)) {
+			OpenBook();
+		}
+	}
+
 	private string FillTextbox(TMP_Text textbox, int charsPerIteration, string text) {
 		string remainingText = text;
+		
+		const int loopLimit = 2000;
+		int i = 0;
 
 		while(remainingText.Length != 0) {
-			// Add text to the textbox
-			string textToAdd = remainingText.Substring(0, charsPerIteration);
-			textbox.text += textToAdd;
 			
-			// Shave off text as needed
-			const int loopLimit = 10000;
-			int i = 0;
-			while(textbox.textInfo.lineCount > maxLinesPerPage) {
+			// Add text to the textbox
+			string textToAdd;
+			if(charsPerIteration > remainingText.Length) {
+				textToAdd = remainingText;
+			} else {
+				textToAdd = remainingText.Substring(0, charsPerIteration);
+			}
+			textbox.text += textToAdd;
 
-				// Remove the last character
-				textbox.text.Remove(textbox.text.Length - 1);
-				i++;
-				if(i > loopLimit) {
-					Debug.LogError("Reached loop limit");
-					break;
+			// Shave off text as needed
+			textbox.ForceMeshUpdate();
+			Debug.Log(textbox.textInfo.lineCount);
+
+			bool overshot = false;
+			if(textbox.textInfo.lineCount > maxLinesPerPage) {
+				overshot = true;
+				
+				while(textbox.textInfo.lineCount > maxLinesPerPage) {
+
+					// Remove the last character
+					textbox.text = textbox.text.Substring(0, textbox.text.LastIndexOf(' '));
+					textbox.ForceMeshUpdate();
+
+					i++;
+					if(i > loopLimit) {
+						Debug.LogError("Reached loop limit");
+						return remainingText;
+					}
 				}
 			}
 
-			return remainingText;
+			if(overshot) {
+				return remainingText;
+			}
+
+			remainingText = text.Substring(textbox.text.Length);
+
+			i++;
+			if(i > loopLimit) {
+				Debug.LogError("Reached loop limit");
+				return remainingText;
+			}
 		}
 		return "";
 	}
