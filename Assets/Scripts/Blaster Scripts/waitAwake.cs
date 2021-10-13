95679454454a42e0c651f713911366fd0da14624
brizbird14 <brizbird14@gmail.com>
2021-10-13 02:09:44 -0400

    Blaster endgame music
    Add music, start adding tweaks for pause and gradient

---
+++ Assets/Scripts/Blaster Scripts/waitAwake.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waitAwake : MonoBehaviour
+{
+    public GameObject endMusicObj;
+    public float audioWait;
+    AudioSource endMusicSource;
+    bool finalVol; // initialized to false
+    void Start() {
+        endMusicSource = endMusicObj.GetComponent<AudioSource>();
+        endMusicSource.enabled = false;
+        //endMusicSource.volume = 0.1f;
+    }
+    void FixedUpdate(){
+        if(Time.timeSinceLevelLoad >= audioWait) {
+            endMusicSource.enabled = true;
+        }
+    }
+}
