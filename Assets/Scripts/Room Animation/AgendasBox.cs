4e779382012ad3ecd3e1ef831cc1436b351a5dfe
ndnestor <nathan.nestor@outlook.com>
2022-08-23 15:45:29 -0400

    Re-added AgendasBox.cs

---
+++ Assets/Scripts/Room Animation/AgendasBox.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgendasBox : MonoBehaviour {
+
+    [SerializeField] private GameObject door;
+
+    public void OnAnimationStart()
+    {
+        door.SetActive(false);
+    }
+    
+    public void OnAnimationFinish()
+    {
+        door.SetActive(true);
+    }
+    
+}
